bde5d069ec9338422a3b12f3e54ca8b28a929520
ttb <ttb@shackspace.de>
2015-11-02 16:59:56 +0100

    remote_test.sh downloads rrd-files

--- remote_test.sh
+++ remote_test.sh
@@ -2,5 +2,8 @@
 
 scp bind_*.py ttb.shack: \
 	&&  ssh ttb.shack /root/bind_graph.py \
+	&& rm -v *.png || true  \
 	&& scp ttb.shack:\*.png . \
+	&& rm -v *.rrd || true  \
+	&& scp ttb.shack:\*.rrd . \
 	&& eog Incoming_Queries-6h.png
